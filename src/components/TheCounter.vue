ad6284d1f70eda50d489ff64302202400f38b25f
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-14 11:01:22 +0200

    created store and manage state in main and used it in other components

---
+++ src/components/TheCounter.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>{{ counting }}</h3>
+</template>
+<script>
+export default {
+  computed: {
+    counting() {
+      return this.$store.state.counter;
+    },
+  },
+};
+</script>
